134e9cfa9f6889a6331ad1ab447ce1c1828cee7c
AntonyA59 <antony.alsberghe@hotmail.fr>
2023-08-08 13:04:52 +0200

    Refactorisation PhoneNumberDirective

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
 class PhoneNumberDirective {
-    constructor(public element: HTMLElement){}
-    init(){
-        this.element.style.borderColor = "red";
+  constructor(public element: HTMLElement) {}
 
-        this.element.addEventListener('input', (event) => {
-            const element = event.target as HTMLInputElement;
-            const value = element.value.replace(/[^\d]/g, '').substring(0,10);
-            const groups: string[] = []
-            for(let i = 0; i< value.length; i+=2){
-                groups.push(value.substring(i, i+2))
-            } 
-
-            element.value =groups.join(' ');
-            console.log(groups);
-        })
+  formatPhoneNumber(element: HTMLInputElement) {
+    const value = element.value.replace(/[^\d]/g, "").substring(0, 10);
+    const groups: string[] = [];
+    for (let i = 0; i < value.length; i += 2) {
+      groups.push(value.substring(i, i + 2));
     }
+    element.value = groups.join(" ");
+  }
+
+  init() {
+    this.element.style.borderColor = "red";
+
+    this.element.addEventListener("input", (event) => {
+      this.formatPhoneNumber(event.target as HTMLInputElement);
+    });
+  }
 }
 
-const element = document.querySelector<HTMLElement>('#mobile-number');
-if(element){
-    const directive = new PhoneNumberDirective(element)
-    directive.init();
+const element = document.querySelector<HTMLElement>("#mobile-number");
+if (element) {
+  const directive = new PhoneNumberDirective(element);
+  directive.init();
 }
