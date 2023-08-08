c629c6b48c66908501c717ad6fe28a66c506d00b
AntonyA59 <antony.alsberghe@hotmail.fr>
2023-08-08 21:38:59 +0200

    Refacto Directives & Create Services folder

--- src/directives/credit-card.directive.ts
+++ src/directives/credit-card.directive.ts
@@ -1,14 +1,19 @@
+import { Formatter } from "../services/formatter";
+
 export class CreditCardDirective{
+
     constructor(public element: HTMLElement) {}
+
     static selector = "[credit-card]"
+
+    willHaveSpaces = true
     
+    formatter:Formatter;
+
     formatCreditCardNumber(element: HTMLInputElement) {
-      const value = element.value.replace(/[^\d]/g, "").substring(0, 16);
-      const groups: string[] = [];
-      for (let i = 0; i < value.length; i += 4) {
-        groups.push(value.substring(i, i + 4));
-      }
-      element.value = groups.join(" ");
+      this.formatter = new Formatter;
+      
+      element.value  = this.formatter.formatNumber(element.value,16,4,this.willHaveSpaces)
     }
     
       init(){
--- src/directives/phone-number.directive.ts
+++ src/directives/phone-number.directive.ts
@@ -1,21 +1,34 @@
+import { Formatter } from "../services/formatter"
+
 export class PhoneNumberDirective {
     constructor(public element: HTMLElement) {}
+
     static selector = "[phone-number]"
-  
-    formatPhoneNumber(element: HTMLInputElement) {
-      const value = element.value.replace(/[^\d]/g, "").substring(0, 10);
-      const groups: string[] = [];
-      for (let i = 0; i < value.length; i += 2) {
-        groups.push(value.substring(i, i + 2));
-      }
-      element.value = groups.join(" ");
+
+    borderColor = "red"
+    
+    willHaveSpaces = true;
+
+    formatter:Formatter;
+    formatPhoneNumber(element: HTMLInputElement) {   
+      this.formatter = new Formatter;
+
+      element.value  = this.formatter.formatNumber(element.value,10,2,this.willHaveSpaces)
     }
   
     init() {
-      this.element.style.borderColor = "red";
-  
+      
+      
+    if(this.element.hasAttribute('with-spaces')){
+      this.willHaveSpaces = this.element.getAttribute('with-spaces') === 'true';
+    }
+
+    if(this.element.hasAttribute('border-color')){
+      this.borderColor = this.element.getAttribute('border-color')!;
+    }
       this.element.addEventListener("input", (event) => {
         this.formatPhoneNumber(event.target as HTMLInputElement);
       });
+      this.element.style.borderColor = this.borderColor;
     }
   }
